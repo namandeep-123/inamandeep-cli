33939f8ee2d7210539fe65c812c6c0cb30d01edd
Namandeep Singh <namandeepsingh557@gmail.com>
2025-01-14 22:18:55 +0530

    remove ascii art below 105 terminal width

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,9 @@ import { promptMenu } from "../src/utils/promptMenu.js";
 
 // Main function to start the CLI
 (async function main() {
-  await displayHeader(); // Display animated ASCII art
-  promptMenu(); // Launch the interactive menu
+  const terminalWidth = process.stdout.columns;
+  if (terminalWidth > 105) {
+    await displayHeader(); // Display animated ASCII art
+  }
+  promptMenu();
 })();
